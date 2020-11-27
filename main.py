d280da62b988e6f414d427d37b22f830669310f7
simons22d <denniskiruku@gmail.com>
2020-11-27 08:51:45 +0300

    timer to -> 1800 sec

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import datetime
 import time
 import requests
 
+
 def time_in_range(start, end, x):
     """Return true if x is in the range [start, end]"""
     if start <= end:
@@ -13,14 +14,14 @@ def time_in_range(start, end, x):
 while True:
     now = datetime.datetime.now()
     start = datetime.time(8, 0, 0)
-    end  = datetime.time(11, 0, 0)
+    end = datetime.time(11, 0, 0)
     hr = int(now.strftime("%H"))
     min = int(now.strftime("%M"))
     sec = int(now.strftime("%S"))
-    x = datetime.time(hr,min,sec)
-    if time_in_range(start,end ,x):
+    x = datetime.time(hr, min, sec)
+    if time_in_range(start, end, x):
         print("its time.")
-        time.sleep(60)
+        time.sleep(60*30)
         requests.post("http://192.168.12.200:9000/send/email/reminder")
     else:
         print("Its Not Time.")
